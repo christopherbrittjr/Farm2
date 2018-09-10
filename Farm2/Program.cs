8782b535a11dc056dfca17ab0d0f0b5b148ffadf
Chris Britt <christopher.brittjr@outlook.com>
2018-09-09 21:33:40 -0400

    Add project files.

---
+++ Farm2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Throwing an exception because I have not implemented the classes.
+            //Farm newFarm = new Farm();
+            try
+            {
+                Console.WriteLine("Farm has not been implement yet");
+
+            }
+            catch (Exception nogo)
+            {
+                Console.WriteLine(nogo.Message);
+            }
+        }
+        
+    }
+ 
+}    
